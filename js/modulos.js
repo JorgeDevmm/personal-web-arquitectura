0c975ee44d249e5ddaf6eb2961c67dc288867a0b
Jorge Monzon <jorge29luis10@gmail.com>
2023-02-18 15:51:15 -0500

    Agregando tema oscuro

--- js/modulos.js
+++ js/modulos.js
@@ -1,1 +1,6 @@
-import {} from './tema_oscuro';
+import temaOscuro from './tema_oscuro.js';
+
+// carga evento al analizar html por completo
+document.addEventListener('DOMContentLoaded', (evento) => {
+  temaOscuro('.tema-oscuro-btn', 'modo-oscuro');
+});
---
+++ js/tema_oscuro.js
@@ -0,0 +1,26 @@
+// pasamos dos parametor el boton y el atributo de los selectores a cambiar
+export default function temaOscuro(btn, classOscuro) {
+  const temaBtn = document.querySelector(btn);
+  const selector = document.querySelectorAll('[data-oscura]'); //estilos por atributo
+
+  let moon = '🌙',
+    sun = ' ☀️';
+
+  document.addEventListener('click', (event) => {
+    if (event.target.matches(btn)) {
+      console.log(temaBtn.textContent);
+
+      if (temaBtn.textContent === moon) {
+        selector.forEach((elemento) => {
+          elemento.classList.add(classOscuro);
+        });
+        temaBtn.textContent = sun;
+      } else {
+        selector.forEach((elemento) => {
+          elemento.classList.remove(classOscuro);
+        });
+        temaBtn.textContent = moon;
+      }
+    }
+  });
+}
